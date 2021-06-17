d8589f0ad3370a1137cb1f0df0cd8fe45ea9f399
Patrick Jusic <patrickjusic96@gmail.com>
2021-06-17 20:28:31 +0200

    Briefly refactor code improving styling and fixing unchecked response data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
 import { useQuery } from "react-query";
+import { useTimer } from "use-timer";
 import "./App.css";
 import {
+  agrorobotica,
   black,
   black0,
   black1,
   black2,
-  agrorobotica,
   green,
   low_green,
   low_red,
 } from "./assets";
-import { useEffect } from "react";
-import { useTimer } from "use-timer";
+import "./keyframes.css";
+import mock from "./mock";
 
 const networkTypes = {
   gsm: "2G",
@@ -24,112 +25,7 @@ const networkTypes = {
 const fetchSignal = async () => {
   if (process.env.NODE_ENV === "development") {
     return new Promise((res, rej) => {
-      res({
-        modem: {
-          "3gpp": {
-            "enabled-locks": [],
-            eps: {
-              "initial-bearer": {
-                "dbus-path": "--",
-                settings: {
-                  apn: "--",
-                  "ip-type": "--",
-                  password: "--",
-                  user: "--",
-                },
-              },
-              "ue-mode-operation": "--",
-            },
-            imei: "867648043678091",
-            "operator-code": "22201",
-            "operator-name": "--",
-            pco: "--",
-            "registration-state": "idle",
-          },
-          cdma: {
-            "activation-state": "--",
-            "cdma1x-registration-state": "--",
-            esn: "--",
-            "evdo-registration-state": "--",
-            meid: "--",
-            nid: "--",
-            sid: "--",
-          },
-          "dbus-path": "/org/freedesktop/ModemManager1/Modem/0",
-          generic: {
-            "access-technologies": [],
-            bearers: [],
-            "carrier-configuration": "default",
-            "carrier-configuration-revision": "--",
-            "current-bands": [
-              "egsm",
-              "dcs",
-              "utran-1",
-              "utran-8",
-              "eutran-1",
-              "eutran-3",
-              "eutran-7",
-              "eutran-8",
-              "eutran-20",
-              "eutran-28",
-            ],
-            "current-capabilities": ["gsm-umts, lte"],
-            "current-modes": "allowed: 2g, 3g, 4g; preferred: 4g",
-            device: "/sys/devices/platform/soc/3f980000.usb/usb1/1-1/1-1.3",
-            "device-identifier": "b11776b4562b297911729d29a96e7256240619ad",
-            drivers: ["qmi_wwan"],
-            "equipment-identifier": "867648043678091",
-            "hardware-revision": "10000",
-            manufacturer: "QUALCOMM INCORPORATED",
-            model: "QUECTEL Mobile Broadband Module",
-            "own-numbers": [],
-            plugin: "quectel",
-            ports: ["cdc-wdm0 (qmi)", "wwan0 (net)"],
-            "power-state": "on",
-            "primary-port": "cdc-wdm0",
-            revision: "EG91EXGAR08A03M1G",
-            "signal-quality": { recent: "no", value: "0" },
-            sim: "/org/freedesktop/ModemManager1/SIM/0",
-            state: "enabled",
-            "state-failed-reason": "--",
-            "supported-bands": [
-              "egsm",
-              "dcs",
-              "utran-1",
-              "utran-8",
-              "eutran-1",
-              "eutran-3",
-              "eutran-7",
-              "eutran-8",
-              "eutran-20",
-              "eutran-28",
-            ],
-            "supported-capabilities": ["gsm-umts, lte"],
-            "supported-ip-families": ["ipv4", "ipv6", "ipv4v6"],
-            "supported-modes": [
-              "allowed: 2g; preferred: none",
-              "allowed: 3g; preferred: none",
-              "allowed: 4g; preferred: none",
-              "allowed: 2g, 3g; preferred: 3g",
-              "allowed: 2g, 3g; preferred: 2g",
-              "allowed: 2g, 4g; preferred: 4g",
-              "allowed: 2g, 4g; preferred: 2g",
-              "allowed: 3g, 4g; preferred: 4g",
-              "allowed: 3g, 4g; preferred: 3g",
-              "allowed: 2g, 3g, 4g; preferred: 4g",
-              "allowed: 2g, 3g, 4g; preferred: 3g",
-              "allowed: 2g, 3g, 4g; preferred: 2g",
-            ],
-            "unlock-required": "sim-pin2",
-            "unlock-retries": [
-              "sim-pin (3)",
-              "sim-pin2 (3)",
-              "sim-puk (10)",
-              "sim-puk2 (10)",
-            ],
-          },
-        },
-      });
+      res(mock);
     });
   }
   const resp = await fetch(
@@ -160,15 +56,19 @@ function App() {
   }, [start]);
 
   const networkLevel = useMemo(() => {
-    return data.modem.generic["signal-quality"].value;
+    if (data) return data?.modem?.generic["signal-quality"]?.value;
   }, [data]);
 
   const operator = useMemo(() => {
-    return data.modem["3gpp"]["operator-name"];
+    if (data) return data?.modem["3gpp"]["operator-name"];
   }, [data]);
 
   const networkType = useMemo(() => {
-    return networkTypes[data.modem.generic["access-technologies"].value[1]];
+    // if (data) return networkTypes[data.modem.generic["access-technologies"].value[1]];
+    if (data)
+      return networkTypes[
+        data?.modem?.generic["access-technologies"][0]?.value
+      ];
   }, [data]);
 
   return (
